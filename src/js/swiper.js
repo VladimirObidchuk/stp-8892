8e92c36179ad06691b335c972131a9ef65a28983
Vladimir <kremench@gmail.com>
2025-05-03 20:59:02 +0300

    swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,12 +1,11 @@
 import Swiper from 'swiper';
-// import Swiper styles
 import 'swiper/css';
 
 const swiper = new Swiper('.gallery-swiper', {
   slidesPerView: 3,
   spaceBetween: 30,
   pagination: {
-    el: '.gallery-pagination',
+    el: '.swiper-scrollbar',
     clickable: true,
   },
 });

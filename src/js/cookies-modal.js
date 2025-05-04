eb9526f5e3d4f1184b9032d3eab1097e0d6dfb33
Vladimir <kremench@gmail.com>
2025-05-04 09:22:27 +0300

    Add block scroll for body

--- src/js/cookies-modal.js
+++ src/js/cookies-modal.js
@@ -2,24 +2,26 @@ const cookiesModal = document.querySelector('[data-accept]');
 const acceptCookies = document.querySelector('[data-cookies-accept]');
 const declineCookies = document.querySelector('[data-cookies-decline]');
 
-if (acceptCookies && declineCookies) {
-  acceptCookies.addEventListener('click', cookiesAccept);
-  declineCookies.addEventListener('click', cookiesDecline);
-} else if (localStorage.getItem('cookies')) {
-  cookiesModal.classList.remove('is-accept');
-  document.removeEventListener('click', cookiesDecline);
-  document.removeEventListener('click', cookiesAccept);
-} else {
-  acceprCookies.addEventListener('click', cookiesAccept);
-  declineCookies.addEventListener('click', cookiesDecline);
-}
+document.addEventListener('DOMContentLoaded', () => {
+  if (!localStorage.getItem('cookies')) {
+    document.body.classList.add('no-scroll'); // Блокуємо прокрутку, якщо модалка активна
+    cookiesModal.classList.add('is-accept');
+  }
+
+  if (acceptCookies && declineCookies) {
+    acceptCookies.addEventListener('click', cookiesAccept);
+    declineCookies.addEventListener('click', cookiesDecline);
+  }
+});
 
 function cookiesAccept() {
   cookiesModal.classList.remove('is-accept');
   localStorage.setItem('cookies', 'accept cookies');
+  document.body.classList.remove('no-scroll');
 }
 
 function cookiesDecline() {
   cookiesModal.classList.remove('is-accept');
   localStorage.setItem('cookies', 'decline cookies');
+  document.body.classList.remove('no-scroll');
 }

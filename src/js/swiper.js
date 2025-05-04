73078edbedc06fa0976a33caa75a9126d120293e
Vladimir <kremench@gmail.com>
2025-05-04 11:31:00 +0300

    add height for coocies

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,11 +1,17 @@
 import Swiper from 'swiper';
 import 'swiper/css';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/navigation';
 
 const swiper = new Swiper('.gallery-swiper', {
   slidesPerView: 1,
   spaceBetween: 10,
-  pagination: {
+  modules: [Navigation],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
     el: '.swiper-scrollbar',
-    clickable: true,
   },
 });
